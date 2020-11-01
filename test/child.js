b70481db5beea0b16a337e464259ff2fe4f0951b
Inrix <Inrixia@gmail.com>
2020-11-01 18:55:49 +1300

    Cleaned up directory structure

--- test/child.js
+++
@@ -1,5 +0,0 @@
-process.on('message', message => {
-    message.hi = 1;
-    console.log(message)
-    process.exit();
-})
--- test/childTwo.js
+++
@@ -1,5 +0,0 @@
-process.on('message', message => {
-    message.lol = 2;
-    console.log(message)
-    process.exit();
-})
