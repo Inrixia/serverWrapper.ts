4cb3d8186a581dbdc3aab7c01012b35e1955bcd6
Inrix <Inrixia@gmail.com>
2021-09-10 00:35:13 +1200

    Removing pkg as its currently broken

--- wrapperHelpers/src/work.ts
+++ wrapperHelpers/src/work.ts
@@ -9,33 +9,13 @@ const references = require("../../tsconfig.json").references;
 for (const { path: refPath } of Object.values<{ path: string }>(references)) {
 	for (const file of listFiles(`${refPath}/dist`).filter((file) => file.endsWith(".js"))) {
 		if (file === "./wrapperHelpers/dist/work.js") continue;
-		// fs.unlink(file, () => {});
-		let content = fs.readFileSync(file, "utf8");
-		let match;
-		let matches = [];
-		while ((match = reqRegex.exec(content)) !== null) {
-			matches.push(match);
-			// This is necessary to avoid infinite loops with zero-width matches
-			if (match.index === reqRegex.lastIndex) reqRegex.lastIndex++;
-		}
-		for (const match of matches) {
-			match.map((matchString, groupIndex) => {
-				if (groupIndex === 0 || matchString === "chalk" || matchString[0] === ".") return;
-				const _path = file.slice(0, file.lastIndexOf("/"));
-				try {
-					content = content
-						.replace(/\.require/g, "_____dotrequire______")
-						.replace(`require("${matchString}")`, `require("${require.resolve(matchString, { paths: [_path] }).replace(/\\/g, "/")}")`)
-						.replace(/\_\_\_\_\_dotrequire\_\_\_\_\_\_/g, ".require")
-						.replaceAll(__dirname, "/snapshot/project");
-				} catch (e) {
-					console.log(e);
-					console.log(file, matchString);
-					process.exit();
-				}
-			});
-		}
-		fs.writeFileSync(file, content);
+		fs.writeFileSync(
+			file,
+			fs
+				.readFileSync(file, "utf8")
+				.replaceAll("@spookelton/wrapperHelpers/modul", "@spookelton/wrapperHelpers/dist/modul")
+				.replaceAll("@spookelton/wrapperHelpers/colors", "@spookelton/wrapperHelpers/dist/colors")
+		);
 	}
 }
 // takes a folder and recursively lists all files
