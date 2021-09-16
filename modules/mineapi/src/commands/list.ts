9546d9bd16adf44d67058035832665dc13b1f85c
huantian <davidtianli@gmail.com>
2021-09-15 20:21:46 -0700

    Convert list to use server status

--- modules/mineapi/src/commands/list.ts
+++ modules/mineapi/src/commands/list.ts
@@ -1,46 +1,39 @@
 // Import Types
 import { Command } from "@spookelton/wrapperHelpers/types";
 
-import { getCore, thread } from ".."
+import { getStatus } from ".."
 import { helpHelper, strOut } from "@spookelton/wrapperHelpers/modul";
 
 export const list: Command = async (message) => {
-	// Execute the list command
-	const wrapperThread = await getCore();
-	await wrapperThread.serverStdin("list\n");
+	const serverStatus = await getStatus();
 
-	// Get the server's response to the command
-	// TODO: Set a timeout for this.
-	const result = await new Promise<RegExpMatchArray>((resolve, reject) => {
-		// TODO: Make sure this format works for relevant minecraft versions.
-		const regex = new RegExp(String.raw
-			`\[\d+:\d+:\d+\] \[Server thread\/INFO\]: There are (\d+) of a max of (\d+) players online: (.+( ,)?)*`
-		);
+	const { maxPlayers, onlinePlayers, samplePlayers } = serverStatus;
+	const onlineMessage = samplePlayers?.length
+						  ? `:\n${samplePlayers.map(x => x.name).join(", ")}`
+						  : ".\nIt is quite lonely.";
 
-		const getMatch = (line: string) => {
-			const maybeMatch = line.match(regex);
-
-			if (maybeMatch === null) return;
-
-			thread.removeListener("serverStdout", getMatch);
-			resolve(maybeMatch);
-		}
-
-		thread.on("serverStdout", getMatch);
-	});
-
-	// Construct a custom message with info extracted from server output
-	const numOnline = result[1];
-	const numMax = result[2];
-	const onlinePlayers: string | undefined = result[3];
-	const onlinePlayersMessage = onlinePlayers ? `:\n${onlinePlayers}` : ".\nIt is quite lonely.";
-
-	const outputMessage = `There are ${numOnline}/${numMax} players online${onlinePlayersMessage}`;
+	const outputMessage = `There are ${onlinePlayers}/${maxPlayers} players online` + onlineMessage;
 
 	// TODO: Make better formatting.
+	// Includes perhaps showing more detailed userinfo, icon, etc.
 	// TODO: Do I have to do something to handle console/ingame output? Ideally there should be no output there, or
 	// just the raw command output? Or maybe we can use the formatted output as well.
-	return strOut(outputMessage);
+	return {
+		console: outputMessage,
+		minecraft: outputMessage,
+		discord: {
+			embeds: [
+				{
+					title: outputMessage,
+					timestamp: Date.now(),
+					author: {
+						name: "",
+						icon_url: "https://cdn.discordapp.com/avatars/300050030923087872/b85c91512bf80b884a75548e71b9a817.webp"
+					},
+				},
+			],
+		}
+	};
 };
 list.help = helpHelper({
 	commandString: "~list",
