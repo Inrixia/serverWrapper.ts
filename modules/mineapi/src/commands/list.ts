10bacf84565a03a33af8e4b09bd0a2a98b49128d
huantian <davidtianli@gmail.com>
2021-09-17 16:31:19 -0700

    change list formatting

--- modules/mineapi/src/commands/list.ts
+++ modules/mineapi/src/commands/list.ts
@@ -1,22 +1,34 @@
 // Import Types
 import { Command } from "@spookelton/wrapperHelpers/types";
 
-import { getStatus } from ".."
+
+import { getCore, getStatus } from ".."
 import { helpHelper, strOut } from "@spookelton/wrapperHelpers/modul";
+import { hex } from "@spookelton/wrapperHelpers/colors";
 
 export const list: Command = async (message) => {
+	const core = await getCore();
+	if (!await core.serverStarted()) return strOut("Server is still starting! Please wait...", "red");
+
 	const serverStatus = await getStatus();
-
 	const { maxPlayers, onlinePlayers, samplePlayers } = serverStatus;
+
+	const titleMessage = `There are ${onlinePlayers}/${maxPlayers} players online:`;
 	const onlineMessage = samplePlayers?.length
-						  ? `:\n${samplePlayers.map(player => player.name).join(", ")}`
-						  : ".\nIt is quite lonely.";
+						  ? samplePlayers.map(player => player.name).join(", ")
+						  : "It is quite lonely.";
 
-	const outputMessage = `There are ${onlinePlayers}/${maxPlayers} players online` + onlineMessage;
-
-	// TODO: Make better formatting.
-	// Show more detailed userinfo (e.g. user head, icon).
-	return strOut(outputMessage)
+	// TODO: Make better formatting. Show more detailed userinfo (e.g. user head, icon).
+	return {
+		minecraft: `${titleMessage} ${onlineMessage}`,
+		console: `${titleMessage} ${onlineMessage}`,
+		discord: {embeds: [{
+			title: titleMessage,
+			description: onlineMessage,
+			timestamp: Date.now(),
+			color: parseInt(hex.green, 16),
+		}]}
+	};
 };
 list.help = helpHelper({
 	commandString: "~list",
