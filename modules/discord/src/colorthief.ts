d7ec07091b8992b52f744a7286cf36180b92c6a3
Scott Gould <greysilly7@gmail.com>
2022-01-16 00:38:56 -0500

    Remove ColorTheif

--- modules/discord/src/colorthief.ts
+++
@@ -1,4 +0,0 @@
-declare module "colorthief" {
-	export function getColor(image: string, quality?: number): Promise<[number, number, number]>;
-	export function getPalette(image: string, colorCount?: number, quality?: number): Promise<[number, number, number][]>;
-}
